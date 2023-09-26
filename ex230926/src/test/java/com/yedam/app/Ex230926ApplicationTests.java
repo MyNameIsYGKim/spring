ff3b5e3d117d2f1541fa1570216b2c826b260759
MyNameIsYGKim <kelen536@gmail.com>
2023-09-26 17:59:52 +0900

    230926 springboot

---
+++ ex230926/src/test/java/com/yedam/app/Ex230926ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yedam.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex230926ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
